b0796173894264bff6f5fa1a14c64e5f7cc56be7
José Diego <JoseHuamani_20@hotmail.com>
2024-09-07 03:12:40 -0500

    build: import the packages to SpringBoot Framework

---
+++ src/main/java/com/acme/pointbarbackend/PointBarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acme.pointbarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointBarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointBarBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/pointbarbackend/PointBarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.pointbarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointBarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
