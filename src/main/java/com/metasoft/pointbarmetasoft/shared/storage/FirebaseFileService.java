ad2e18c426c0c65a94393d8fb52bd9789608e77b
Claudio Moreno Rosales <u20191e800@upc.edu.pe>
2024-11-15 10:52:37 -0500

    feat: properties updates

--- src/main/java/com/metasoft/pointbarmetasoft/shared/storage/FirebaseFileService.java
+++ src/main/java/com/metasoft/pointbarmetasoft/shared/storage/FirebaseFileService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 
 @Service
 public class FirebaseFileService {
-    @Value("${FIREBASE_CREDENTIALS}")
+    @Value("${firebase.credentials}")
     private String firebaseCredentials;
     private Storage storage;
 
